5bce7cf86481392836554ccb1feccddaccff195f
CHEE MING HENG <17010372@rp.edu.sg>
2018-08-14 10:08:20 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/receiverapp/BatteryReceiver.java
@@ -0,0 +1,16 @@
+package sg.edu.rp.c346.receiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by 17010372 on 14/8/2018.
+ */
+
+public class BatteryReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
